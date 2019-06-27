e77b5a0534ca696ac6452e9f115a6dc7bce73f4f
raspberrypisig <raspberrypisig@gmail.com>
2019-06-27 16:55:53 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# If this is run from SSH, your connection will disconnect when this script is done.
+
 git clone https://github.com/raspberrypisig/pizero-usb-hid-keyboard
 cd pizero-usb-hid-keyboard
 ./setup-hid-modules.sh
